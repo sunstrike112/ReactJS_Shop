f526b1d68d24ad127ea6c5ce231c78b64a2bb281
PhapTDQ <phaptran112@gmail.com>
2023-02-21 23:38:17 +0700

    160. Schema Validation with Yup

--- Vite_Shopee/src/pages/Register/Register.tsx
+++ Vite_Shopee/src/pages/Register/Register.tsx
@@ -1,12 +1,10 @@
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import { getRules } from 'src/utils/rules'
+import { schema, Schema } from 'src/utils/rules'
 import Input from 'src/components/Input'
-interface FormData {
-  email: string
-  password: string
-  confirm_password: string
-}
+import { yupResolver } from '@hookform/resolvers/yup'
+
+type FormData = Schema
 
 export default function Register() {
   const {
@@ -14,9 +12,9 @@ export default function Register() {
     handleSubmit,
     getValues,
     formState: { errors }
-  } = useForm<FormData>()
-
-  const rules = getRules(getValues)
+  } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
 
   const onSubmit = handleSubmit(
     (data) => {
@@ -43,7 +41,6 @@ export default function Register() {
                 className='mt-8'
                 errorMessage={errors.email?.message}
                 placeholder='Email'
-                rules={rules.email}
               />
               <Input
                 name='password'
@@ -52,7 +49,6 @@ export default function Register() {
                 className='mt-2'
                 errorMessage={errors.password?.message}
                 placeholder='Password'
-                rules={rules.password}
                 autoComplete='on'
               />
               <Input
@@ -62,7 +58,6 @@ export default function Register() {
                 className='mt-2'
                 errorMessage={errors.confirm_password?.message}
                 placeholder='Confirm Password'
-                rules={rules.confirm_password}
                 autoComplete='on'
               />
               <div className='mt-2'>
