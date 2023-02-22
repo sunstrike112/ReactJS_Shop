d01c36fc1851314e063940de3f00be0a77f610b5
PhapTDQ <phaptran112@gmail.com>
2023-02-22 14:19:39 +0700

    162. Call API Register

--- Vite_Shopee/src/pages/Register/Register.tsx
+++ Vite_Shopee/src/pages/Register/Register.tsx
@@ -1,8 +1,11 @@
-import { Link } from 'react-router-dom'
-import { useForm } from 'react-hook-form'
-import { schema, Schema } from 'src/utils/rules'
-import Input from 'src/components/Input'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { registerAccount } from 'src/apis/auth.api'
+import Input from 'src/components/Input'
+import { schema, Schema } from 'src/utils/rules'
 
 type FormData = Schema
 
@@ -10,21 +13,23 @@ export default function Register() {
   const {
     register,
     handleSubmit,
-    getValues,
     formState: { errors }
   } = useForm<FormData>({
     resolver: yupResolver(schema)
   })
 
-  const onSubmit = handleSubmit(
-    (data) => {
-      // console.log(data)
-    },
-    (data) => {
-      const password = getValues('password')
-      console.log(password)
-    }
-  )
+  const registerAccountMutation = useMutation({
+    mutationFn: (body: Omit<FormData, 'confirm_password'>) => registerAccount(body)
+  })
+
+  const onSubmit = handleSubmit((data) => {
+    const body = omit(data, ['confirm_password'])
+    registerAccountMutation.mutate(body, {
+      onSuccess: (data) => {
+        console.log(data)
+      }
+    })
+  })
 
   return (
     <div className='bg-orange'>
