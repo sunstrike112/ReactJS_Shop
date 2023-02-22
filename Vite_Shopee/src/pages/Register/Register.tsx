d8166b230368cc4f0914ccec30fb8eb114d420c0
PhapTDQ <phaptran112@gmail.com>
2023-02-22 15:41:03 +0700

    163. Handle error 422

--- Vite_Shopee/src/pages/Register/Register.tsx
+++ Vite_Shopee/src/pages/Register/Register.tsx
@@ -5,7 +5,9 @@ import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { registerAccount } from 'src/apis/auth.api'
 import Input from 'src/components/Input'
+import { ResponseApi } from 'src/types/utils.type'
 import { schema, Schema } from 'src/utils/rules'
+import { isAxiosUnprocessableEntityError } from 'src/utils/utils'
 
 type FormData = Schema
 
@@ -13,6 +15,7 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors }
   } = useForm<FormData>({
     resolver: yupResolver(schema)
@@ -27,6 +30,31 @@ export default function Register() {
     registerAccountMutation.mutate(body, {
       onSuccess: (data) => {
         console.log(data)
+      },
+      onError: (error) => {
+        if (isAxiosUnprocessableEntityError<ResponseApi<Omit<FormData, 'confirm_password'>>>(error)) {
+          const formError = error.response?.data.data
+          if (formError) {
+            Object.keys(formError).forEach(key => {
+              setError(key as keyof Omit<FormData, 'confirm_password'>, {
+                message: formError[key as keyof Omit<FormData, 'confirm_password'>],
+                type: 'Server'
+              })
+            })
+          }
+          // if (formError?.email) {
+          //   setError('email', {
+          //     message: formError.email,
+          //     type: 'Server'
+          //   })
+          // }
+          // if (formError?.password) {
+          //   setError('password', {
+          //     message: formError.password,
+          //     type: 'Server'
+          //   })
+          // }
+        }
       }
     })
   })
--- Vite_Shopee/src/types/utils.type.ts
+++ Vite_Shopee/src/types/utils.type.ts
@@ -1,4 +1,4 @@
-export interface ResponseApi<Data>{
+export interface ResponseApi<Data> {
   message: string
   data?: Data
 }
