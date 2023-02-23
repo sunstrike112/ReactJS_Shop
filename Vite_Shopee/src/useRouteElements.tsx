1d8e2c6bb1155a33ee2d9e3bc6c58add24a2d382
PhapTDQ <phaptdq@bap.jp>
2023-02-23 10:17:19 +0700

    170. Setup Protected Route

--- Vite_Shopee/src/useRouteElements.tsx
+++ Vite_Shopee/src/useRouteElements.tsx
@@ -1,37 +1,70 @@
 import React from 'react'
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import RegisterLayout from './layouts/RegisterLayout'
-import Login from './pages/login'
+import Login from './pages/Login'
 import ProductList from './pages/ProductList'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
+
+
+function ProtectedRoute() {
+  const isAuthenticated = true
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  const isAuthenticated = false
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
     {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: 'profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
+    },
+    {
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: 'login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: 'register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        }
+      ]
+    },
+    {
       path: '/',
+      index: true,
       element: (
         <MainLayout>
           <ProductList />
         </MainLayout>
       )
     },
-    {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
-    },
-    {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
-    }
   ])
   return routeElements
 }
