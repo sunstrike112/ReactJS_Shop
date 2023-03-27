acc4683b09fe76c5d698c408f67e198493277a34
PhapTDQ <phaptran112@gmail.com>
2023-03-27 16:25:37 +0700

    199. Define API purchases and Clear data ReactQuery when logout

--- Vite_Shopee/src/useRouteElements.tsx
+++ Vite_Shopee/src/useRouteElements.tsx
@@ -1,14 +1,15 @@
-import React, { useContext } from 'react'
+import path from 'src/constants/path'
+import { useContext } from 'react'
 import { Navigate, Outlet, useRoutes } from 'react-router-dom'
+import { AppContext } from './contexts/app.context'
 import MainLayout from './layouts/MainLayout'
 import RegisterLayout from './layouts/RegisterLayout'
-import Login from './pages/login'
+import Login from './pages/Login'
 import ProductList from './pages/ProductList'
+import Profile from './pages/Profile'
 import Register from './pages/Register'
 import ProductDetail from './pages/ProductDetail'
-import Profile from './pages/Profile'
-import { AppContext } from './contexts/app.context'
-import path from './constants/path'
+import Cart from './pages/Cart'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
@@ -17,25 +18,12 @@ function ProtectedRoute() {
 
 function RejectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
+
   return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
 }
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
-    {
-      path: '',
-      element: <ProtectedRoute />,
-      children: [
-        {
-          path: path.profile,
-          element: (
-            <MainLayout>
-              <Profile />
-            </MainLayout>
-          )
-        }
-      ]
-    },
     {
       path: '',
       element: <RejectedRoute />,
@@ -59,6 +47,28 @@ export default function useRouteElements() {
       ]
     },
     {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: path.profile,
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        },
+        {
+          path: path.cart,
+          element: (
+            <MainLayout>
+              <Cart />
+            </MainLayout>
+          )
+        }
+      ]
+    },
+    {
       path: path.productDetail,
       element: (
         <MainLayout>
@@ -67,7 +77,7 @@ export default function useRouteElements() {
       )
     },
     {
-      path: path.home,
+      path: '',
       index: true,
       element: (
         <MainLayout>
