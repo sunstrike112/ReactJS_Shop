7eab5032a89f34f3df8c801a0686230e27ce643b
PhapTDQ <phaptran112@gmail.com>
2023-05-27 16:07:28 +0700

    240. Update Mock Service Worker for another API

--- Vite_Shopee/src/utils/__test__/http.test.ts
+++ Vite_Shopee/src/utils/__test__/http.test.ts
@@ -1,6 +1,7 @@
 import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+import { access_token_1s, refresh_token_1000days } from 'src/msw/auth.msw'
 import { describe, expect, it, beforeEach } from 'vitest'
-import { getAccessTokenFromLS, getRefreshTokenFromLS, setAccessTokenToLS, setRefreshTokenToLS } from '../auth'
+import { setAccessTokenToLS, setRefreshTokenToLS } from '../auth'
 import { Http } from '../http'
 
 describe('http axios', () => {
@@ -9,11 +10,6 @@ describe('http axios', () => {
     localStorage.clear()
     http = new Http().instance
   })
-  const access_token_1s =
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xNVQxNDowMzoyMy41NzdaIiwiaWF0IjoxNjcxMTEzMDAzLCJleHAiOjE2NzExMTMwMDR9.-gQIpbbKFlRqBlpiiAOBD4puP8jcMtZ2lobXPcy1zmU'
-  const refresh_token_1000days =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNmY5MzVlNWZkYzVmMDM3ZTZmNjhkMyIsImVtYWlsIjoiZDNAZ21haWwuY29tIiwicm9sZXMiOlsiVXNlciJdLCJjcmVhdGVkX2F0IjoiMjAyMi0xMi0xNVQxNDowNTozNS41MTVaIiwiaWF0IjoxNjcxMTEzMTM1LCJleHAiOjE3NTc1MTMxMzV9.OHDBqBjhih1fgNe6-mWo0PQ-IcukNz4ljlXUCxM-8V8'
-
   it('Gọi API', async () => {
     // Không nên đụng đến thư mục apis
     // Vì chúng ta test riêng file http thì chỉ "nên" dùng http thôi
@@ -27,8 +23,8 @@ describe('http axios', () => {
     // Nên có 1 cái account test
     // và 1 server test
     await http.post('login', {
-      email: 'd7@gmail.com',
-      password: '123123'
+      email: 'd3@gmail.com',
+      password: 'useruser'
     })
     const res = await http.get('me')
     expect(res.status).toBe(HttpStatusCode.Ok)
